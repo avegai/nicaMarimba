31d94ffd3c19a3a9e468806f7b46ca69a4539043
Alejandro Vega <avegaicaza@gmail.com>
2024-12-18 19:29:55 +0100

    started developing, added the first keyboard samples

--- sketch.js
+++ sketch.js
@@ -1,18 +1,26 @@
-let mySound;
-let mySound2;
+let atabal;
+let marimbaChord;
+let marimbaC;
+let marimbaE;
+let marimbaG;
 let delay;
 let filter;
 let started = false;
-let path = "assets/SC_NM_marimba_panoramic_Emaj.wav";
-let path2 = "assets/SC_NM_93_perc_loop_high_atabal_3_4_time.wav";
+let path = "assets/SC_NM_93_perc_loop_high_atabal_3_4_time.wav";
+let path2 = "assets/SC_NM_156_marimba_monimbo_arp_tail_3_4_time_Cmaj.wav";
+let path3 = "assets/SC_NM_marimba_single_note_C.wav";
+let path4 = "assets/SC_NM_marimba_single_note_E.wav";
+let path5 = "assets/SC_NM_marimba_single_note_G.wav";
 let img;
 let button;
 let paneo;
 
-
 function preload() {
-  mySound = loadSound(path);
-  mySound2 = loadSound(path2);
+  atabal = loadSound(path);
+  marimbaChord = loadSound(path2);
+  marimbaC = loadSound(path3);
+  marimbaE = loadSound(path4);
+  marimbaG = loadSound(path5);
   
   img = loadImage("assets/bg_img.jpg");
 }
@@ -24,28 +32,28 @@ function setup() {
 
   //call start sound when the button is pressed.
   button.mousePressed(startSound);
-  // fft = new p5.FFT();
 
   //create modules and initialize parameters.
   delay = new p5.Delay();
   delay.delayTime(random(0.1, 10));
   delay.feedback(0.5);
   filter = new p5.LowPass();
-  mySound.loop();
-  mySound2.loop();
+  // atabal.loop();
+  // marimbaChord.loop();
 
   //connect modules
-  mySound.disconnect();
+  atabal.disconnect();
   filter.disconnect();
-  mySound.connect(filter);
+  atabal.connect(filter);
   filter.connect(delay);
   
-  mySound2.disconnect();
-  mySound2.connect(delay);
+  marimbaChord.disconnect();
+  marimbaChord.connect(delay);
 
   imageMode(CENTER);
-  // img.filter(GRAY);
+  img.filter(GRAY);
 }
+
 
 function draw() {
   // frameRate(13);
@@ -53,7 +61,7 @@ function draw() {
   img.resize(1000, 500);
   image(img, width / 2, height / 2);
   
-  mySound2.rate(random(0, 2));
+  marimbaChord.rate(random(0, 2));
 
   let frequency = map(mouseY, 0, height, 0, 20000);
   // let resonance = map(mouseY, height, 0, 5, 25);
@@ -61,19 +69,29 @@ function draw() {
   filter.freq(frequency);
     
   let paneo = map(mouseX, 0, width, -1, 1);
-  mySound.pan(paneo); 
-  mySound2.pan(paneo);
+  atabal.pan(paneo); 
+  marimbaChord.pan(paneo);
   
 }
 
 function startSound() {
   if (!started) {
-    mySound.play();
-    mySound2.play();
+    // atabal.play();
+    // marimbaChord.play();
     started = true;
   } else {
-    mySound.stop();
-    mySound2.stop();
+    atabal.stop();
+    marimbaChord.stop();
     started = false;
+  }
+}
+
+function keyPressed() {
+  if (key === 'a') {
+    atabal.play();
+  }
+
+  if (key === 's'){
+    marimbaChord.play();
   }
 }
